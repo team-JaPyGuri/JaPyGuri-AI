5e3651b51c19ecca751a4678bc01fed587458f0d
Han_geonhui <hgh6945@inha.edu>
2024-11-29 16:51:14 +0900

    commit_11/29

--- self_supervised_train2.py
+++ self_supervised_train2.py
@@ -10,17 +10,15 @@ from torch.utils.data import DataLoader
 
 from dataset.make_dataset import *
 from logger import Logger
-from model.autoencoder import *
+from model.unet import *
 from torch.nn import MSELoss
 from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
 
-def val(args, epoch, encoder, decoder, criterion, val_loader, logger=None):
-
-    encoder.eval()  
-    decoder.eval() 
+def val(args, epoch, feature_extractor, constructor, criterion, val_loader, logger=None):
+    feature_extractor.eval()  
+    constructor.eval() 
 
     num_progress, next_print = 0, args.print_freq
-
     train_loss = 0.0
     num_loader = len(val_loader)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -29,8 +27,8 @@ def val(args, epoch, encoder, decoder, criterion, val_loader, logger=None):
         inputs = inputs.to(device)
         targets = targets.to(device)
 
-        latent, size = encoder(inputs)
-        output = decoder(latent, size)
+        latent, size = feature_extractor(inputs)
+        output = constructor(latent, size)
 
         loss = criterion(output, targets)
         train_loss += loss.item()
@@ -49,10 +47,9 @@ def val(args, epoch, encoder, decoder, criterion, val_loader, logger=None):
 
     return train_loss / num_loader
 
-def train(args, epoch, encoder, decoder, criterion, optimizer, train_loader, logger=None):
-
-    encoder.train()  
-    decoder.train() 
+def train(args, epoch, feature_extractor, constructor, criterion, optimizer, train_loader, logger=None):
+    feature_extractor.train()  
+    constructor.train() 
 
     num_progress, next_print = 0, args.print_freq
     train_loss = 0.0
@@ -65,8 +62,8 @@ def train(args, epoch, encoder, decoder, criterion, optimizer, train_loader, log
 
         optimizer.zero_grad()
 
-        latent, size = encoder(inputs)
-        output = decoder(latent, size)
+        latent, feature = feature_extractor(inputs)
+        output = constructor(latent, feature)
 
         loss = criterion(output, targets)
         loss.backward()
@@ -91,27 +88,26 @@ def train(args, epoch, encoder, decoder, criterion, optimizer, train_loader, log
 def run(args):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+
     # Random Seed
     if args.seed is not None:
         random.seed(args.seed)
         torch.manual_seed(args.seed)
         torch.backends.cudnn.deterministic = True
 
-    model_encoder = encoder(in_channel=3)
-    model_decoder = decoder(out_channel=3)
-    load_path = '/path/to/your/pretrained/model.pth'
-    model_encoder.load_state_dict(torch.load(load_path, map_location=device)) # load encoder
+    feature_ext = feature_extractor(in_channel=3)
+    construct = reconstructor(out_channel=3)
 
     criterion = MSELoss()
-    optimizer = torch.optim.Adam(list(model_encoder.parameters()) + list(model_decoder.parameters()), lr=args.lr)
+    optimizer = torch.optim.Adam(list(feature_ext.parameters()) + list(construct.parameters()), lr=args.lr)
     scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=2)
 
     # CUDA
-    model_encoder.to(device)
-    model_decoder.to(device)
+    feature_ext.to(device)
+    construct.to(device)
 
     # Dataset
-    train_loader, val_loader = make_target_loader(batch_size=args.batch_size, patch_flag=False)
+    train_loader, val_loader = make_target_loader(batch_size=args.batch_size, noise_factor=0.25, patch_ratio=-1.0)
 
     # Logger
     logger = Logger(os.path.join(args.result, 'log.txt'), epochs=args.epochs, dataset_size=len(train_loader.dataset), float_round=5)
@@ -122,14 +118,14 @@ def run(args):
     save_dir = os.path.join(args.result, 'checkpoints')
     for epoch in range(args.epochs):
         # Train
-        train_loss = train(args, epoch, model_encoder, model_decoder, criterion, optimizer, train_loader, logger)
+        train_loss = train(args, epoch, feature_ext, construct, criterion, optimizer, train_loader, logger)
 
         # Validation
         if epoch % args.val_freq == 0 or epoch == args.epochs - 1:
-            val_loss = val(args, epoch, model_encoder, model_decoder, criterion, val_loader, logger)
+            val_loss = val(args, epoch, feature_ext, construct, criterion, val_loader, logger)
             os.makedirs(save_dir, exist_ok=True)
 
-            model_state_dict = model_encoder.module.state_dict() if torch.cuda.device_count() > 1 else model_encoder.state_dict()
+            model_state_dict = feature_ext.module.state_dict() if torch.cuda.device_count() > 1 else feature_ext.state_dict()
             if epoch % 2 == 1:
                 torch.save(model_state_dict, os.path.join(save_dir, '{}.pth'.format(epoch)))
 
@@ -145,7 +141,7 @@ if __name__ == '__main__':
     parser.add_argument('--mask_dir', default='./Data/Qupath2/mask', help='path to mask dir')  # 마스크 디렉토리 경로
     parser.add_argument('--workers', default=4, type=int, help='number of data loading workers')
     # Training Arguments
-    parser.add_argument('--epochs', default=300, type=int, help='number of total epochs to run') 
+    parser.add_argument('--epochs', default=200, type=int, help='number of total epochs to run') 
     parser.add_argument('--batch_size', default=8, type=int, help='mini-batch size')  
     parser.add_argument('--lr', default=0.001, type=float, help='initial learning rate',
                         dest='lr')  
@@ -155,8 +151,12 @@ if __name__ == '__main__':
     parser.add_argument('--print_freq', default=1000, type=int, help='print frequency')
     parser.add_argument('--print_confusion_mat', default=False,
                         action='store_true')  # 혼동 행렬 출력
-    parser.add_argument('--result', default='noise_ssl', type=str, help='path to results')
+    parser.add_argument('--result', default='unet_SSL', type=str, help='path to results')
     parser.add_argument('--tag', default=None, type=str)
+    # WandB Arguments
+    parser.add_argument('--use_wandb', action='store_true', help='whether to use WandB for logging')
+    parser.add_argument('--wandb_project', default='unet_SSL', type=str, help='WandB project name')
+    parser.add_argument('--run_name', default=None, type=str, help='WandB run name')
     args = parser.parse_args()
 
     # Paths setting
